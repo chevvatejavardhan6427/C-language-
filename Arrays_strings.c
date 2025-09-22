90fc6fe2888333ed69277676b8ebf4b9ab48eb89
chevvatejavardhan6427 <chevvatejavardhan@gmail.com>
2025-09-22 16:05:13 +0530

    Create Arrays_strings.c

---
+++ Arrays_strings.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main (){
+	char names[4][25]={0};
+	int size=sizeof(names)/sizeof(names[0]);
+	for(int i=0;i<size;i++){
+		printf("enter  name : ");
+		fgets(names[i],sizeof(names[i]),stdin);					names[i][strlen(names[i])-1]='\0';
+		
+	}
+	for(int i=0;i<size;i++){
+		printf(" %s\n",names[i]);
+	}
+	return 0;
+}
