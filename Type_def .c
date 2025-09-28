78da71621f3caf53cf1a21d25cd665315d52f01b
chevvatejavardhan6427 <chevvatejavardhan@gmail.com>
2025-09-28 13:39:15 +0530

    Create Type_def .c
    It increases  readability of our code .

---
+++ Type_def .c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+typedef int num;
+typedef char name[50];
+
+int main(){
+	num a=2;
+	num b=7;
+	num k=a+b;
+	printf("%d\n",k);
+	name nick="vinay";
+	printf("%s",nick);
+	return 0;
+	
+}
