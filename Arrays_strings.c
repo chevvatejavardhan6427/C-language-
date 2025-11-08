570ed44638afa96f576b864353e6cbd1fdf54d93
chevvatejavardhan6427 <chevvatejavardhan@gmail.com>
2025-11-08 07:29:15 +0530

    Update Arrays_strings.c
    sting arrays are very use full strcture , we can organize any thing we want

--- Arrays_strings.c
+++ Arrays_strings.c
@@ -5,7 +5,8 @@ int main (){
 	int size=sizeof(names)/sizeof(names[0]);
 	for(int i=0;i<size;i++){
 		printf("enter  name : ");
-		fgets(names[i],sizeof(names[i]),stdin);					names[i][strlen(names[i])-1]='\0';
+		fgets(names[i],sizeof(names[i]),stdin);					
+		names[i][strlen(names[i])-1]='\0';
 		
 	}
 	for(int i=0;i<size;i++){
